5a6ee64d7a27a8dc1d8891080f052cb72b991f5f
Kawai Mikey Wong <wongk8@uw.edu>
2013-10-28 01:11:09 -0700

    added fade-in animation

--- js/address-book.js
+++ js/address-book.js
@@ -34,3 +34,54 @@ function sortObjArray(objArray, propName) {
     });
 } //sortObjArray()
 
+
+/* render()
+    Uses the html template and creates a person div for each 
+    entry in the entries array.
+
+    entries         array of objects to render
+
+    returns undefined
+*/
+function render(entries) {
+    var person = $(".template");
+    var addressBook = $(".address-book");
+
+    addressBook.hide();
+    addressBook.empty();
+
+    // for each entry, clone the template, set entry information, and 
+    //append to address book
+    for(var i = 0; i < entries.length; i++) {
+        var newPerson = person.clone();
+
+        newPerson.find(".first").html(entries[i].first);
+        newPerson.find(".last").html(entries[i].last);
+        newPerson.find(".title").html(entries[i].title);
+        newPerson.find(".dept").html(entries[i].dept);
+        newPerson.find(".pic").attr("src", entries[i].pic);
+        newPerson.find(".pic").attr("alt", "picture of " + entries[i].first);
+
+        newPerson.removeClass("template");
+
+        addressBook.append(newPerson);
+    }
+
+    //fade-in animation
+    addressBook.fadeIn()
+}
+
+$(".sort-ui .btn").click(function() {
+    var sortBtn = $(this); 
+    var sortby = sortBtn.attr('data-sortby');
+    sortObjArray(Employees.entries, sortby);
+    render(Employees.entries);
+
+    sortBtn.siblings(".active").removeClass("active");
+    sortBtn.addClass("active");
+});
+
+$(function() {
+    sortObjArray(Employees.entries, "last")
+    render(Employees.entries)
+});
